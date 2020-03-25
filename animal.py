e74a7589b131adf4504dc2fc5d8b73bd2f5b5beb
Kerem <kerem0comert@gmail.com>
2020-03-25 15:39:14 +0300

    Animal?

---
+++ animal.py
@@ -0,0 +1,7 @@
+class Animal:
+    def __init__(self, aNo, aGender, aDoB, aColor, environment):
+        self.aNo = aNo
+        self.aGender = aGender
+        self.aDoB = aDoB
+        self.aColor = aColor
+        self.environment = environment
